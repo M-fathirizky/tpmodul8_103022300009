9cdbc47ce134871864cb67a88abc48a81f0b15f9
M-fathirizky <fathirrizky636@gmail.com>
2025-04-12 22:47:24 +0800

    Menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_103022300009
+{
+    internal class CovidConfig
+    {
+        public Config config;
+
+        public const String filePath = "covid_config.json";
+
+        
+        public CovidConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+
+
+        private Config ReadConfigFile()
+        {
+            String configJsonData = File.ReadAllText(filePath);
+            config = JsonSerializer.Deserialize<Config>(configJsonData);
+            return config;
+        }
+
+        private void SetDefault()
+        {
+            config = new Config
+            {
+                satuan_suhu = "celcius",
+                batas_hari_demam = 14,
+                pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+            };
+        }
+
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            String jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public void UbahSatuan()
+        {
+            if (this.config.satuan_suhu == "celcius")
+            {
+                this.config.satuan_suhu = "fahrenheit";
+            }
+            else
+            {
+                this.config.satuan_suhu = "celcius";
+            }
+            WriteNewConfigFile();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using tpmodul8_103022300009;
+
+namespace tpmodul8_103022300009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CovidConfig covidConfig = new CovidConfig();
+
+            string satuan = covidConfig.config.satuan_suhu;
+
+            Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {satuan}: ");
+            double suhu = double.Parse(Console.ReadLine());
+
+            Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman? ");
+            int hariDemam = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+
+            bool suhuNormal = false;
+
+            if (satuan == "celcius")
+            {
+                suhuNormal = suhu >= 36.5 && suhu <= 37.5;
+            }
+            else if (satuan == "fahrenheit")
+            {
+                suhuNormal = suhu >= 97.7 && suhu <= 99.5;
+            }
+
+            bool hariAman = hariDemam < covidConfig.config.batas_hari_demam;
+
+            if (suhuNormal && hariAman)
+            {
+                Console.WriteLine(covidConfig.config.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(covidConfig.config.pesan_ditolak);
+            }
+
+
+        }
+    }
+}
